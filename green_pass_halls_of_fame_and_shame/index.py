a1bfaf611258e9b098b03bfc7ae58eeb2f77a63a
brtzz-mtt <brtzz.mtt@gmail.com>
2021-10-26 10:46:09 +0200

    fixed grid.css cloning

--- green_pass_halls_of_fame_and_shame/index.py
+++ green_pass_halls_of_fame_and_shame/index.py
@@ -1,12 +1,14 @@
-from package.configuration import BASE_TITLE, DEBUG_MODE
+from package.configuration import BASE_PATH, BASE_TITLE, DEBUG_MODE
 
 from flask import Flask, render_template
+from shutil import copyfile
 
 def flask_template_render(template: str = 'index.html',
     title: str = BASE_TITLE,
     args: dict = {},
     debug: bool = DEBUG_MODE
 ) -> str:
+    copyfile(BASE_PATH + '../../grid.css', BASE_PATH + '../static/grid.css')
     return render_template(template,
         title = title,
         args = args,
